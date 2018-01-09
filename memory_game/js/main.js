a7d1f4f90b35cba7b917458e3f375f34dd6abe0f
Milanova <Bilyana.Milanova@disney.com>
2018-01-08 20:38:09 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,31 @@
+console.log("Up and running!");
+var cards =[
+  {rank: "queen",
+  suit: "hearts",
+  cardImage:"images/queen-of-hearts.png"},
+  {rank:"queen",
+  suit:"diamonds",
+  cardImage: "images/queen-of-diamonds.png"},
+  {rank:"king",
+  suit: "hearts",
+  cardImage:"images/king-of-hearts.png"},
+  {rank: "king",
+  suite:"diamonds",
+  cardImage:"images/king-of-diamonds.png"}
+];
+var cardsInPlay=[];
+var checkForMatch = function() {
+if (cardsInPlay.length===2)
+ if(cardsInPlay[0]===cardsInPlay[1])
+{console.log("You found a macth!");}
+else {
+alert("Sorry, try again!");}
+}
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId].rank)
+  console.log(cards[cardId].cardImage)
+  console.log(cards[cardId].suit)
+cardsInPlay.push(cards[cardId].rank);
+  checkForMatch();}
+  flipCard(0);
+  flipCard(2);
