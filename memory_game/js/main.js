dae045b105cd9c6725733572b94024108a8bbd0b
Milanova <Bilyana.Milanova@disney.com>
2018-01-11 18:31:06 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -13,19 +13,36 @@ var cards =[
   suite:"diamonds",
   cardImage:"images/king-of-diamonds.png"}
 ];
-var cardsInPlay=[];
-var checkForMatch = function() {
+
+var cardsInPlay = [];
+var checkForMatch = function(cardId) {
+  //document.getElementById(cardId).setAttribute('src',cards[cardId].cardImage);
 if (cardsInPlay.length===2)
- if(cardsInPlay[0]===cardsInPlay[1])
-{console.log("You found a macth!");}
+ if(cardsInPlay[0]===cardsInPlay[1]) {
+alert("You found a macth!");}
 else {
 alert("Sorry, try again!");}
 }
-var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId].rank)
-  console.log(cards[cardId].cardImage)
-  console.log(cards[cardId].suit)
+var flipCard = function () {
+  var cardId = this.getAttribute('data-id');
+//console.log("User flipped " + cards[cardId].rank)
+//  console.log(cards[cardId].cardImage)
+this.setAttribute('src',cards[cardId].cardImage);
 cardsInPlay.push(cards[cardId].rank);
-  checkForMatch();}
-  flipCard(0);
-  flipCard(2);
+  checkForMatch(cardId);}
+  //create a board playing function
+var createBoard = function() {
+    for (var i=0; i < cards.length; i++){
+    var cardElement = document.createElement('img'); // image element will substitue a card in play?
+  cardElement.setAttribute('src','images/back.png');
+  cardElement.setAttribute('data-id',i);
+  // add event listener to know when card is flipped and execute the flip card function
+cardElement.addEventListener('click', flipCard)
+  //document.getElementsByTagName('img')[0].addEventListener('click', flipCard);
+  // append the card to the game board
+  document.getElementById('game-board').appendChild(cardElement);
+}
+  }
+    createBoard();
+  //add event listener for refreshing the page after we played one game
+document.getElementsByTagName('button').addEventListener('click',reload());
