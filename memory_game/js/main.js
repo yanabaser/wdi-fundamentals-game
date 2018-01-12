0bd3b5caa2f469d367d447a66d6d9ffe48463013
Milanova <Bilyana.Milanova@disney.com>
2018-01-11 23:45:20 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -15,34 +15,29 @@ var cards =[
 ];
 
 var cardsInPlay = [];
+
 var checkForMatch = function(cardId) {
-  //document.getElementById(cardId).setAttribute('src',cards[cardId].cardImage);
 if (cardsInPlay.length===2)
  if(cardsInPlay[0]===cardsInPlay[1]) {
 alert("You found a macth!");}
 else {
 alert("Sorry, try again!");}
 }
+
 var flipCard = function () {
   var cardId = this.getAttribute('data-id');
-//console.log("User flipped " + cards[cardId].rank)
-//  console.log(cards[cardId].cardImage)
 this.setAttribute('src',cards[cardId].cardImage);
 cardsInPlay.push(cards[cardId].rank);
   checkForMatch(cardId);}
-  //create a board playing function
+
 var createBoard = function() {
     for (var i=0; i < cards.length; i++){
     var cardElement = document.createElement('img'); // image element will substitue a card in play?
   cardElement.setAttribute('src','images/back.png');
   cardElement.setAttribute('data-id',i);
-  // add event listener to know when card is flipped and execute the flip card function
+
 cardElement.addEventListener('click', flipCard)
-  //document.getElementsByTagName('img')[0].addEventListener('click', flipCard);
-  // append the card to the game board
   document.getElementById('game-board').appendChild(cardElement);
 }
   }
     createBoard();
-  //add event listener for refreshing the page after we played one game
-document.getElementsByTagName('button').addEventListener('click',reload());
